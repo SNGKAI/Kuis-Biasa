15e083f0291f692a177180c1a00a3b69d2bb8b07
SNGKAI <niger20sangkai@gmail.com>
2026-03-22 16:57:24 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+        numb: 1,
+        questions: "Apa fungsi utama bahasa pemograman HTML?",
+        answer: "A. Menentukan struktur dan konten dari halaman web",
+        options: [
+            "A. Menentukan struktur dan konten dari halaman web",
+            "B. Menambahkan gaya dan efek visual pada halaman web",
+            "C. Mengubah posisi yang salah pada web tertentu",
+            "D. Memproses data di server"
+        ]
+    },
+
+    {
+        numb: 2,
+        questions: "Apa kepanjangan dari HTML?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. Hyper Transfer Markup Language",
+            "B. Hypertext Markdown Language",
+            "C. HyperText Markup Language",
+            "D. Hyper Transfer Markdown Language"
+        ]
+    },
+    
+    {
+        numb: 3,
+        questions: "Berikut, komponen bahasa C++, kecuali....",
+        answer: "B. Pengelolaan file dan folder",
+        options: [
+            "A. Operator aritmatika",
+            "B. Pengelolaan file dan folder",
+            "C. Pointer dam refrensi",
+            "D. Struct dan Union",
+        ]
+    },
+    {
+        numb: 4,
+        questions: "Tujuan utama dari sistem operasi pada komputer yaitu....",
+        answer: "D. Mengelola perangkat keras dan perangkat lunak serta menyediakan antarmuka",
+        options: [
+            "A. Memanipulasi data menggunakan algoritma kompleks",
+            "B. Meningkatkan kecepatan prosesor dan memori komputer",
+            "C. Mengembangkan aplikasi perangkat lunak baru",
+            "D. Mengelola perangkat keras dan perangkat lunak serta menyediakan antarmuka"
+        ]
+    },
+    {
+        numb: 5,
+        questions: "Perangkat keras pada komputer yang berfungsi sebagai otak komputer adalah....",
+        answer: "D. CPU",
+        options: [
+            "A. GPU",
+            "B. Monitor",
+            "C. Hardisk",
+            "D. CPU"
+        ]
+    },
+    {
+        numb: 6,
+        questions: "Planet tata surya yang rotasinya agak laen, macam putarnya sangat miring gitu adalah.... ",
+        answer: "D. Uranus",
+        options: [
+            "A. Jupiter",
+            "B. Bumi",
+            "C. Mars",
+            "D. Uranus"
+        ]
+    },
+    {
+        numb: 7,
+        questions: "Jika hari ini adalah hari selasa, maka besok nya hari....",
+        answer: "B. Kamis",
+        options: [
+            "A. Rabu",
+            "B. Kamis",
+            "C. Jumat",
+            "D. Sabtu"
+        ]
+    },
+    {
+        numb: 8,
+        questions: "Angka berikutnya dari pola 3, 6, 9, 12, 15, ... adalah...",
+        answer: "A. 18",
+        options: [
+            "A. 18",
+            "B. 20",
+            "C. 23",
+            "D. 16"
+        ]
+    },
+    {
+        numb: 9,
+        questions: "Angka satuan dari 999^998 adalah...",
+        answer: "C. 1",
+        options: [
+            "A. 9",
+            "B. 4",
+            "C. 1",
+            "D. 2"
+        ]
+    },
+    {
+        numb: 10,
+        questions: "Hasil dari 6 : 2(1+2) = .... (JANGAN PAKE KALKULATOR!!!)",
+        answer: "C. 9",
+        options: [
+            "A. 1",
+            "B. 7",
+            "C. 9",
+            "D. 4"
+        ]
+    }
+];
